bd5728545c8ad61801da25cf5980e7f29ba98710
JustAida <suradet40@gmail.com>
2022-03-25 11:02:50 +0700

    Simplify legislators_by_zipcode function

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,6 +1,6 @@
 require "csv"
 require "google/apis/civicinfo_v2"
-
+require "erb"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
@@ -11,14 +11,11 @@ def legislator_by_zipcode(zip)
   civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: "country",
       roles: ["legislatorUpperBody", "legislatorLowerBody"]
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    ).officials
   rescue
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
@@ -32,6 +29,8 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read("form_letter.html")
+
 contents.each do |row|
   name = row[:first_name]
 
@@ -39,5 +38,8 @@ contents.each do |row|
 
   legislators = legislator_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub("FIRST_NAME", name)
+  personal_letter.gsub!("LEGISLATORS", legislators)
+
+  puts personal_letter
 end
