0f222409b929226e7465b1042d758d609f93309e
JustAida <suradet40@gmail.com>
2022-03-26 10:30:01 +0700

    Create a method to check registered hours

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,6 +1,7 @@
 require "csv"
 require "google/apis/civicinfo_v2"
 require "erb"
+require "time"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
@@ -43,6 +44,13 @@ def clean_phone_number(phone_number)
   end
 end
 
+def most_register_hours(hours_counter)
+  highest_count = hours_counter.sort_by { |_, v| v }.reverse[0][1]
+  most_register_hours = hours_counter.select { |_, v| v == highest_count }
+  most_register_hours = most_register_hours.map { |hour| hour[0] }
+  most_register_hours.join(", ")
+end
+
 puts "EventManager Initialized."
 
 contents = CSV.open(
@@ -53,17 +61,25 @@ contents = CSV.open(
 
 template_letter = File.read("form_letter.erb")
 erb_template = ERB.new template_letter
+hours_counter = Hash.new(0)
 
 contents.each do |row|
   # id = row[0]
-  name = row[:first_name]
+  # name = row[:first_name]
   # zipcode = clean_zipcode(row[:zipcode])
   # legislators = legislator_by_zipcode(zipcode)
 
   # form_letter = erb_template.result(binding)
 
   # save_thank_you_letter(id, form_letter)
-  phone_number = clean_phone_number(row[:homephone])
 
-  puts "#{name} #{phone_number}"
+  # phone_number = clean_phone_number(row[:homephone])
+  # puts "#{name} #{phone_number}"
+
+  register_time = Time.strptime(row[:regdate], "%D %H:%M")
+  register_hour = register_time.hour
+  hours_counter[register_hour] += 1
 end
+
+most_register_hours = most_register_hours(hours_counter)
+puts "We should run more ads during these hours: #{most_register_hours}."
